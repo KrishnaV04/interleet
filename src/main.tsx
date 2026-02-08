ad7fe81d2d9b879b4890a95df95840c8a16b537e
Radhakrishna Vempati <vempatir@uci.edu>
2026-02-07 20:56:41 -0800

    added gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Handle GitHub Pages SPA redirect from 404.html
+const params = new URLSearchParams(window.location.search);
+const redirectPath = params.get('p');
+if (redirectPath) {
+  window.history.replaceState(null, '', redirectPath);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
